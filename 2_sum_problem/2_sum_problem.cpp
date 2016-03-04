0368203812c6f231728e0e36155ab7db19d75cd7
gbhutra <mail.gbhutra@gmail.com>
2016-03-04 22:54:10 +0000

    final version with comments

--- 2_sum_problem/2_sum_problem.cpp
+++ 2_sum_problem/2_sum_problem.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
 #include<vector>
 
+/*compiler flags
+    std=c++11
+    
+    to compile :
+    g++ -std=c++11 2_sum_problem.cpp -o soln
+    
+    to run
+    ./soln
+*/
+
 void print2Sums(std::vector<int>& arr, int k)   {
     std::vector<bool> hash(false,k);
     for (auto i: arr)   {
         if (i<k)    {
             int temp=k-i;
             if(hash[temp]==1)   {
-                std::cout<<temp<<", "<<i;
+                std::cout<<"("<<temp<<", "<<i<<"), ";
                 hash[temp] = false;
             }
             else
@@ -17,7 +27,7 @@ void print2Sums(std::vector<int>& arr, int k)   {
 }
 
 int main()  {
-    std::vector<int> arr {1,2,3,4,5,6,7,8,9};
+    std::vector<int> arr {1,2,3,3,4,4,4,6,7,8,9,1,8,8,4,5,6,7,8,9};
     print2Sums(arr,10);
     return 0;
 }
